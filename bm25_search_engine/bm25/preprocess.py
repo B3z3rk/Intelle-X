6ce7901777d0427ffc2ce4e06a5827c488ed2a10
GizellePaisley <101910007+B3z3rk@users.noreply.github.com>
2025-05-04 17:47:36 -0500

    Update preprocess.py

--- bm25_search_engine/bm25/preprocess.py
+++ bm25_search_engine/bm25/preprocess.py
@@ -1,18 +1,51 @@
+# import re
+# from nltk.corpus import stopwords
+# from nltk.tokenize import word_tokenize
+# from nltk.stem import PorterStemmer
+# import nltk
+
+# nltk.download('punkt')
+# nltk.download('stopwords')
+
+# def preprocess_text(text):
+#     ps = PorterStemmer()
+#     text = text.lower()
+#     text = re.sub(r'[^a-zA-Z\s]', '', text)
+#     words = word_tokenize(text)
+#     stop_words = set(stopwords.words('english'))
+#     # Stem words that are not stopwords
+#     words = [ps.stem(word) for word in words if word not in stop_words]
+#     return " ".join(words)
+
 import re
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-from nltk.stem import PorterStemmer
-import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk import pos_tag
 
-nltk.download('punkt')
-nltk.download('stopwords')
+stop_words = set(stopwords.words('english'))
+lemmatizer = WordNetLemmatizer()
+
+ALLOWED_POS = {'NN', 'NNS', 'NNP', 'NNPS', 'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ', 'JJ', 'JJR', 'JJS'}
+
+SYNONYM_MAP = {
+    "study": ["research", "experiment"],
+    "result": ["finding", "outcome"],
+    "theory": ["model", "framework"]
+}
 
 def preprocess_text(text):
-    ps = PorterStemmer()
     text = text.lower()
-    text = re.sub(r'[^a-zA-Z\s]', '', text)
+    text = re.sub(r'[^a-z0-9\s]', '', text)
     words = word_tokenize(text)
-    stop_words = set(stopwords.words('english'))
-    # Stem words that are not stopwords
-    words = [ps.stem(word) for word in words if word not in stop_words]
-    return " ".join(words)
+    tagged = pos_tag(words)
+    filtered = [lemmatizer.lemmatize(w, 'v') for w, tag in tagged if tag in ALLOWED_POS and w not in stop_words]
+    return " ".join(filtered)
+
+def expand_query(query):
+    terms = preprocess_text(query).split()
+    expanded = []
+    for term in terms:
+        expanded.append(term)
+        expanded.extend(SYNONYM_MAP.get(term, []))
+    return " ".join(expanded)
