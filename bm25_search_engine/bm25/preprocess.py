3a1115ceb11ad4feffd10feb4de9d33175358ca8
GizellePaisley <101910007+B3z3rk@users.noreply.github.com>
2025-05-04 17:48:04 -0500

    Update preprocess.py

--- bm25_search_engine/bm25/preprocess.py
+++ bm25_search_engine/bm25/preprocess.py
@@ -1,22 +1,3 @@
-# import re
-# from nltk.corpus import stopwords
-# from nltk.tokenize import word_tokenize
-# from nltk.stem import PorterStemmer
-# import nltk
-
-# nltk.download('punkt')
-# nltk.download('stopwords')
-
-# def preprocess_text(text):
-#     ps = PorterStemmer()
-#     text = text.lower()
-#     text = re.sub(r'[^a-zA-Z\s]', '', text)
-#     words = word_tokenize(text)
-#     stop_words = set(stopwords.words('english'))
-#     # Stem words that are not stopwords
-#     words = [ps.stem(word) for word in words if word not in stop_words]
-#     return " ".join(words)
-
 import re
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
